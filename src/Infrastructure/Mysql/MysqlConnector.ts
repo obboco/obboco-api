ecb0d1620749179917a05fbf3aae2911ac7712a2
Albert Playà Vidal <albertplayavidal@gmail.com>
2022-08-26 18:36:44 +0200

    Add mysql port configuration

--- src/Infrastructure/Mysql/MysqlConnector.ts
+++ src/Infrastructure/Mysql/MysqlConnector.ts
@@ -3,6 +3,7 @@ import mysql, { Pool } from 'mysql2/promise';
 export async function mysqlConnection(): Promise<Pool> {
   return mysql.createPool({
     host: process.env.MYSQL_HOST,
+    port: parseInt(process.env.MYSQL_PORT),
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
