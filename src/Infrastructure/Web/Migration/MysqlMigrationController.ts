9f6311528d24e21f83e567d6a1b78867ea77e70e
Albert Playà Vidal <albertplayavidal@gmail.com>
2022-08-29 09:26:55 +0200

    Configurate properly mysql credentials for migrtations

--- src/Infrastructure/Web/Migration/MysqlMigrationController.ts
+++ src/Infrastructure/Web/Migration/MysqlMigrationController.ts
@@ -10,12 +10,12 @@ export class MysqlMigrationController implements Controller {
     var mysql = require('mysql');
 
     var connection = mysql.createPool({
-      connectionLimit: 10,
       host: process.env.MYSQL_HOST,
-      port: process.env.MYSQL_PORT,
+      port: parseInt(process.env.MYSQL_PORT),
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE
+      database: process.env.MYSQL_DATABASE,
+      connectionLimit: 10
     });
 
     try {
