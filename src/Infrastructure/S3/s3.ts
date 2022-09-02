b0fcc2abf7e88d8feb96abb93fcddd6b44e45ba9
Albert Playà Vidal <albertplayavidal@gmail.com>
2022-09-02 18:45:12 +0200

    Configure image storage properly

--- src/Infrastructure/S3/s3.ts
+++ src/Infrastructure/S3/s3.ts
@@ -1,67 +1,54 @@
 import { Ulid } from './../../Domain/Shared/ulid';
-const AWS = require('aws-sdk');
-var multer = require('multer');
-var multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
 require('dotenv').config();
 
 const credentials = {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_KEY
+  accessKeyId: process.env.STORAGE_ACCESS_KEY_ID,
+  secretAccessKey: process.env.STORAGE_SECRET_KEY
 };
+const bucketName = process.env.STORAGE_BUCKET_NAME;
+const spacesEndpoint = new aws.Endpoint(process.env.STORAGE_SPACES_ENDPOINT);
 
-//const useLocal = process.env.NODE_ENV !== 'production';
-
-const bucketName = process.env.AWS_BUCKET_NAME;
-
-const s3client = new AWS.S3({
+const s3 = new aws.S3({
   credentials,
-  /**
-   * When working locally, we'll use the Localstack endpoints. This is the one for S3.
-   * A full list of endpoints for each service can be found in the Localstack docs.
-   */
-  endpoint: process.env.API_FILES,
-  /**
-   * Including this option gets localstack to more closely match the defaults for
-   * live S3. If you omit this, you will need to add the bucketName to the `Key`
-   * property in the upload function below.
-   *
-   * see: https://github.com/localstack/localstack/issues/1180
-   */
-  s3ForcePathStyle: true
+  endpoint: spacesEndpoint
 });
 
-// upload to S3 storage
-export const uploadFiles = (req, res, next) => {
+export const uploadFiles = (request, response, next) => {
   const activityImageId = Ulid.create();
+
   const upload = multer({
-    limits: { files: 1 },
     storage: multerS3({
-      s3: s3client,
+      limits: { files: 1 },
+      s3: s3,
       bucket: bucketName,
+      acl: 'public-read',
       metadata: function (req, file, cb) {
         cb(null, { fieldName: activityImageId.value });
       },
-      key: function (req, file, cb) {
+      key: function (request, file, cb) {
+        console.log(file);
         cb(null, activityImageId.value + '.jpg');
       }
     })
   }).single('activity_photo');
 
-  // Custom error handling for multer
-  upload(req, res, (error) => {
+  upload(request, response, (error) => {
     if (error instanceof multer.MulterError)
-      return res.status(400).json({
+      return response.status(400).json({
         message: 'Upload unsuccessful',
         errorMessage: error.message,
         errorCode: error.code
       });
 
     if (error)
-      return res.status(500).json({
+      return response.status(500).json({
         message: 'Error occured',
         errorMessage: error.message
       });
-    res.locals.activityImageId = activityImageId.value;
+    response.locals.activityImageId = activityImageId.value;
     next();
   });
 };
