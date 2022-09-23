1f4992beffe5caf725ae5eac12f7c478a518cb8e
Albert Playà Vidal <albertplayavidal@gmail.com>
2022-09-23 14:38:56 +0200

    Check deploy

--- src/Application/Activity/activityRepository.ts
+++ src/Application/Activity/activityRepository.ts
@@ -4,7 +4,7 @@ import { Activity } from '../../Domain/Activity';
 export interface ActivityRepository {
   add(activity: Activity): Promise<void>;
   update(activity: Activity): Promise<void>;
+  delete(activityId: Ulid): Promise<void>;
   get(activityId: Ulid): Promise<Activity>;
   getByPartnerId(partnerId: Ulid): Promise<Activity[]>;
-  delete(activityId: Ulid): Promise<void>;
 }
